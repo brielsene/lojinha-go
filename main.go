fee0349760d3add0fd8c80656dd553baa86d1b9a
Gabriel Sene <sene300@gmail.com>
2023-08-16 12:40:21 -0300

    feat: main Modularizado com apenas subida do servidor, e chamada das rotas

--- main.go
+++ main.go
@@ -1,68 +1,12 @@
 package main
 
 import (
-	"database/sql"
+	"gabriel/routes"
 	"net/http"
-	"text/template"
-
-	_ "github.com/lib/pq"
 )
 
-func conectaComBancoDeDados() *sql.DB {
-	conexao := "user=postgres dbname=alura_loja password=root host=localhost sslmode=disable"
-	db, err := sql.Open("postgres", conexao)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	return db
-
-}
-
-type Produto struct {
-	Id              int
-	Nome, Descricao string
-	Preco           float64
-	Quantidade      int
-}
-
-var temp = template.Must(template.ParseGlob("templates/*.html"))
-
 func main() {
-	http.HandleFunc("/", index)
+
+	routes.CarregaRotas()
 	http.ListenAndServe(":8000", nil)
-}
-
-func index(w http.ResponseWriter, r *http.Request) {
-	db := conectaComBancoDeDados()
-
-	selectDeTodosOsProdutos, err := db.Query("select * from produtos")
-	if err != nil {
-		panic(err.Error())
-	}
-
-	p := Produto{}
-	produtos := []Produto{}
-
-	for selectDeTodosOsProdutos.Next() {
-		var id, quantidade int
-		var nome, descricao string
-		var preco float64
-
-		err = selectDeTodosOsProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
-		if err != nil {
-			panic(err.Error())
-		}
-		p.Id = id
-		p.Nome = nome
-		p.Descricao = descricao
-		p.Preco = preco
-		p.Quantidade = quantidade
-
-		produtos = append(produtos, p)
-	}
-
-	temp.ExecuteTemplate(w, "Index", produtos)
-	db.Close()
-
 }
