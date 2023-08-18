3dc65d4b455e790b57e94290cf6c8a228b276737
Gabriel Sene <sene300@gmail.com>
2023-08-18 13:52:14 -0300

    feat: Adicionando método para deletar produto por ID

--- models/produtos.go
+++ models/produtos.go
@@ -56,3 +56,15 @@ func CriaNovoProduto(nome, descricao string, preco float64, quantidade int) {
 	defer db.Close()
 
 }
+
+func DeletaProduto(id string) {
+	db := db.ConectaComBancoDeDados()
+
+	deletarOProduto, err := db.Prepare("delete from produtos where id = $1")
+	if err != nil {
+		panic(err.Error())
+	}
+	deletarOProduto.Exec(id)
+	defer db.Close()
+
+}
